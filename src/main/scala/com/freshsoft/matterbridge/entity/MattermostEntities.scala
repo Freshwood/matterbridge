7001cec6147962be33c2de978587825959ac469f
Tobias Frischholz <tfrischholz@kabelmail.de>
2016-07-10 01:16:24 +0200

    Refactored whole actor implementation  * Set next version to 1.2  * Introduced new actor which is adding a single gif result from nine gag  * Make sure the correct actor flow is used

--- src/main/scala/com/freshsoft/matterbridge/entity/MattermostEntities.scala
+++ src/main/scala/com/freshsoft/matterbridge/entity/MattermostEntities.scala
@@ -5,6 +5,7 @@ import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
 import akka.http.scaladsl.model.Uri.Query
 import spray.json.DefaultJsonProtocol
 
+
 /**
 	* Here are all matterbridge defined entities
 	*/
@@ -16,9 +17,7 @@ object MattermostEntities {
 
 	case class OutgoingResponse(text: String)
 
-	case class StartNineGagIntegration(command: String, worker: ActorRef)
-
-	case class StartNineGagGifSearch(command: String)
+	case class NineGagResolveCommand(worker: ActorRef)
 
 	case class NineGagGifResult(key: String = "", gifUrl: String = "")
 
