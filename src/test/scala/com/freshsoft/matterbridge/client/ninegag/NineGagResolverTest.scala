c66b66882381b0e1eb97e87238e2e066e29fa07f
Tobias Frischholz <tfrischholz@kabelmail.de>
2016-07-14 07:44:48 +0200

    Added sbt coveralls plugin to get a proper code coverage analysis [4]  * Added more unit tests

---
+++ src/test/scala/com/freshsoft/matterbridge/client/ninegag/NineGagResolverTest.scala
@@ -0,0 +1,30 @@
+package com.freshsoft.matterbridge.client.ninegag
+
+import akka.actor.{ActorSystem, Props}
+import akka.testkit.{TestKit, TestProbe}
+import com.freshsoft.matterbridge.entity.MattermostEntities.NineGagResolveCommand
+import org.scalatest.{MustMatchers, WordSpecLike}
+
+/**
+	* Created by Freshwood on 14.07.2016.
+	*/
+class NineGagResolverTest extends TestKit(ActorSystem("testSystem")) with WordSpecLike with MustMatchers {
+
+	val expectedMessage = "http://9gag.com/funny/"
+
+	"The nine gag resolver actor" must {
+		"send the next url to the worker actor" in {
+			// Creation of the TestActorRef
+			val probe = TestProbe()
+			val source = system.actorOf(Props[NineGagResolver])
+			val dest = system.actorOf(Props[NineGagWorker])
+
+			source ! NineGagResolveCommand(probe.testActor)
+			probe.expectMsg(expectedMessage)
+			probe.forward(dest)
+		}
+
+	}
+
+
+}
