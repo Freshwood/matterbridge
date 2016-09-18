bc38ef8a7aada7bf75933108ab1865167ed1a609
Tobias Frischholz <tfrischholz@kabelmail.de>
2016-09-19 01:31:57 +0200

    Add support for rss atom feeds [2]

--- src/main/scala/com/freshsoft/matterbridge/client/rss/RssReaderWorkerActor.scala
+++ src/main/scala/com/freshsoft/matterbridge/client/rss/RssReaderWorkerActor.scala
@@ -13,7 +13,7 @@ import net.ruippeixotog.scalascraper.browser.JsoupBrowser
 import net.ruippeixotog.scalascraper.dsl.DSL.Extract._
 import net.ruippeixotog.scalascraper.dsl.DSL._
 
-import scala.xml.XML
+import scala.xml.{NodeSeq, XML}
 
 /**
 	* The rss reader worker actor fetch the rss feed and build a Message for the rssReaderSenderActor
@@ -47,11 +47,18 @@ class RssReaderWorkerActor extends Actor with WithActorContext {
 		*
 		* @param actualPubDate The actual rss feed item article time
 		* @param lastScanDate  The last actor run time saved in a model
+		* @param isRssFeed Indicator if it is an rss feed or an atom feed
 		* @return true when the actual rss item (pubDate) is new
 		*/
-	private def isArticleNew(actualPubDate: String, lastScanDate: String) = {
-		val parsedTime = (x: String) => OffsetDateTime.parse(x, DateTimeFormatter.RFC_1123_DATE_TIME)
-		parsedTime(actualPubDate).isAfter(parsedTime(lastScanDate))
+	private def isArticleNew(actualPubDate: String, lastScanDate: String, isRssFeed: Boolean) = {
+		val rssFeedTime = (x: String) => OffsetDateTime.parse(x, DateTimeFormatter.RFC_1123_DATE_TIME)
+		val atomFeedTime = (x: String) => OffsetDateTime.parse(x, DateTimeFormatter.ISO_DATE_TIME)
+
+		if (isRssFeed) {
+			rssFeedTime(actualPubDate).isAfter(rssFeedTime(lastScanDate))
+		} else {
+			atomFeedTime(actualPubDate).isAfter(rssFeedTime(lastScanDate))
+		}
 	}
 
 	/**
@@ -78,43 +85,45 @@ class RssReaderWorkerActor extends Actor with WithActorContext {
 		*/
 	private def buildRssModel(rssConfig: RssFeedConfigEntry, content: String): Option[RssReaderIncomingModel] = {
 
-		val allRssModels = getRawRssModels(rssConfig, content)
-
-		val rssModels = allRssModels.filter(m => isArticleNew(m.pubDate, rssConfig.lastScanTime))
-		if (rssModels.nonEmpty) rssConfig.lastScanTime = DateTime.now.toRfc1123DateTimeString()
-
-		Some(RssReaderIncomingModel(rssConfig, rssModels))
-	}
-
-	private def getRawRssModels(rssConfig: RssFeedConfigEntry, rawRssContent: String): List[RssReaderModel] = {
-
 		try {
-			val xml = XML.loadString(rawRssContent)
+			val xml = XML.loadString(content)
 			val items = xml \\ "item"
 			val entries = xml \\ "entry"
 
-			if (items.nonEmpty) {
-				(for {
-					i <- items
-					title = (i \ "title").text
-					link = (i \ "link").text
-					pubDate = (i \ "pubDate").text
-					description = (i \ "description").text
-					imageLink = extractImageFromContent(description)
-				} yield RssReaderModel(title, link, pubDate, description, imageLink, rssConfig.name)).toList
-			} else {
-				(for {
-					i <- entries
-					title = (i \ "title").text
-					link = (i \ "link").text
-					pubDate = (i \ "updated").text
-					description = (i \ "summary").text
-					imageLink = extractImageFromContent(description)
-				} yield RssReaderModel(title, link, pubDate, description, imageLink, rssConfig.name)).toList
-			}
+			val isRssFeed = items.nonEmpty
+
+			val allRssModels = if (isRssFeed) getRssFeedModels(rssConfig, items) else getAtomFeedModels(rssConfig, entries)
+
+			val rssModels = allRssModels.filter(m => isArticleNew(m.pubDate, rssConfig.lastScanTime, isRssFeed))
+			if (rssModels.nonEmpty) rssConfig.lastScanTime = DateTime.now.toRfc1123DateTimeString()
+
+			Some(RssReaderIncomingModel(rssConfig, rssModels))
+
 		} catch {
-			case e: Throwable => log.error(s"Could not parse rss content $rawRssContent", e)
-				List()
+			case e: Throwable => log.error(s"Could not parse rss content $content", e)
+				None
 		}
+	}
+
+	private def getRssFeedModels(rssConfig: RssFeedConfigEntry, rssNodeSeq: NodeSeq) = {
+		(for {
+			i <- rssNodeSeq
+			title = (i \ "title").text
+			link = (i \ "link").text
+			pubDate = (i \ "pubDate").text
+			description = (i \ "description").text
+			imageLink = extractImageFromContent(description)
+		} yield RssReaderModel(title, link, pubDate, description, imageLink, rssConfig.name)).toList
+	}
+
+	private def getAtomFeedModels(rssConfig: RssFeedConfigEntry, atomNodeSeq: NodeSeq) = {
+		(for {
+			i <- atomNodeSeq
+			title = (i \ "title").text
+			link = (i \ "link").text
+			pubDate = (i \ "updated").text
+			description = (i \ "summary").text
+			imageLink = extractImageFromContent(description)
+		} yield RssReaderModel(title, link, pubDate, description, imageLink, rssConfig.name)).toList
 	}
 }
