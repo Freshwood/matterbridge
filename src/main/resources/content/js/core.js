4e407a51fe531c0edde5220c63fe22ac8e9385bc
Tobias Frischholz <tfrischholz@kabelmail.de>
2017-03-29 00:09:11 +0200

    Added global error handler

--- src/main/resources/content/js/core.js
+++ src/main/resources/content/js/core.js
@@ -1,4 +1,37 @@
-var MB = MB || {};
+var MB = MB || {
+        errorHandler: function (event, jqxhr, settings, thrownError) {
+            console.log("Could not communicate: " + thrownError);
+        }
+    };
+
+Vue.component('landing', {
+    template: '#home-template',
+    props: {
+        url: {
+            type: String,
+            required: true
+        }
+    },
+    data: function () {
+        return {
+            text: "This is a sample text",
+            categories: []
+        }
+    },
+    methods: {
+        saveCategories: function (categories) {
+            this.categories = categories;
+            console.log(categories);
+        }
+    },
+    created: function () {
+        var vm = this;
+        $.get({
+                  url: vm.url + 'category',
+                  success: vm.saveCategories
+              })
+    }
+});
 
 MB.core = {
 
@@ -15,14 +48,31 @@ MB.core = {
      * Navigation: HOME, 9GAG, CODING_LOVE, RSS, BOT
      */
     init: function () {
+        var navs = MB.core.navs;
         this.vue = new Vue({
                                el: '#contentVue',
                                data: {
-                                   navigation: 'HOME'
+                                   navigation: 'HOME',
+                                   url: '/'
                                },
                                methods: {
                                    switchNavigation: function (newNav) {
                                        this.navigation = newNav;
+                                   },
+                                   isHome: function () {
+                                       return this.navigation === navs.home;
+                                   },
+                                   isNineGag: function () {
+                                       return this.navigation === navs.nineGag;
+                                   },
+                                   isCodingLove: function () {
+                                       return this.navigation === navs.codingLove;
+                                   },
+                                   isRss: function () {
+                                       return this.navigation === navs.rss;
+                                   },
+                                   isBot: function () {
+                                       return this.navigation === navs.bot;
                                    }
                                }
                            });
