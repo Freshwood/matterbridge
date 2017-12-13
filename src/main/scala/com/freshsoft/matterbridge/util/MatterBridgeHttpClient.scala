05210832d3b48519453ac0a77f9da29e567c4d6b
Tobias Frischholz <tfrischholz@kabelmail.de>
2017-12-13 22:29:39 +0100

    Introduced akka http rest client

--- src/main/scala/com/freshsoft/matterbridge/util/MatterBridgeHttpClient.scala
+++ src/main/scala/com/freshsoft/matterbridge/util/MatterBridgeHttpClient.scala
@@ -1,13 +1,13 @@
 package com.freshsoft.matterbridge.util
 
 import akka.event.{Logging, LoggingAdapter}
-import akka.http.scaladsl.Http
 import akka.http.scaladsl.coding.Gzip
 import akka.http.scaladsl.model._
 import akka.stream.scaladsl.Sink
 import akka.util.ByteString
 import com.freshsoft.matterbridge.server.MatterBridgeContext
 import model.MatterBridgeEntities.{ISlashCommandJsonSupport, IncomingResponse}
+import net.softler.client.ClientRequest
 import spray.json._
 
 import scala.concurrent.Future
@@ -27,20 +27,18 @@ object MatterBridgeHttpClient extends ISlashCommandJsonSupport with MatterBridge
     * @return Nothing => only logs the result
     */
   def postToIncomingWebhook(url: String, incomingResponse: IncomingResponse): Future[Unit] =
-    Http().singleRequest(
-      HttpRequest(uri = url,
-                  method = HttpMethods.POST,
-                  headers = Nil,
-                  entity = HttpEntity(incomingResponse.toJson.toString)
-                    .withContentType(MediaTypes.`application/json`))) map {
-      case HttpResponse(StatusCodes.OK, _, _, _) =>
-        log.info(s"Successfully send data to $url with data ${incomingResponse.toJson.toString}")
-
-      // Something went wrong while sending information to incoming token url
-      case _ =>
-        log.warning(
-          s"Could not send data to token url $url with data ${incomingResponse.toJson.toString}")
-    }
+    ClientRequest(url)
+      .entity(HttpEntity(incomingResponse.toJson.toString))
+      .asJson
+      .post()
+      .map(_.response)
+      .map {
+        case HttpResponse(StatusCodes.OK, _, _, _) =>
+          log.info(s"Successfully send data to $url with data ${incomingResponse.toJson.toString}")
+        case _ =>
+          log.warning(
+            s"Could not send data to token url $url with data ${incomingResponse.toJson.toString}")
+      }
 
   /**
     * Get a raw http result from the provided url
@@ -49,7 +47,7 @@ object MatterBridgeHttpClient extends ISlashCommandJsonSupport with MatterBridge
     * @return Raw HttpResponse UTF-8 conform String as a future
     */
   def getUrlContent(url: String): Future[String] =
-    Http().singleRequest(HttpRequest(uri = url)) flatMap {
+    ClientRequest(url).get().map(_.response) flatMap {
 
       case HttpResponse(StatusCodes.OK, headers, entity, _) =>
         val isGzipContent =
